aab0c8797449738cdbc1a8868f48c89bfd4b4386
manologue <manomoumi@gmail.com>
2024-05-31 14:56:34 +0100

    designDbPart1 (creation of db with table users, products and stocks with their respective controller, model, request and response files)

---
+++ app/Models/Product.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Product extends Model
+{
+    use HasFactory;
+
+
+    protected $fillable = [
+        'name',
+    ];
+
+
+
+    public function createdBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function updatedBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
