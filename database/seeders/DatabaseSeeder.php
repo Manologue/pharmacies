25d33a357c2a745b6b0c4b1ec284efbb3d1bbf23
christian-Nitcheu <christiannitcheu97@gmail.com>
2024-06-01 14:42:41 +0100

    same stage

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,7 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(10)->create();
+         \App\Models\Product::factory(10)->create();
+         \App\Models\Stock::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
